88818221796d50315d32fccc5aeac09730427f4b
Firdo.dev <83067641+firdodev@users.noreply.github.com>
2022-12-28 15:07:55 +0100

    Enemy patrolling

---
+++ Assets/Scripts/Enemy/EnemyPatrolling.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolling : MonoBehaviour
+{
+   [SerializeField] float moveSpeed = 1f;
+
+    [Header("References")]
+    public Rigidbody2D enemyRB;
+    public BoxCollider2D enemyBoxCollider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsFacingRight()){
+            //MoveRight
+            enemyRB.velocity = new Vector2(moveSpeed,0f);
+        }else{
+            //MoveLeft
+            enemyRB.velocity = new Vector2(-moveSpeed,0f);
+        }
+    }
+
+    private bool IsFacingRight(){
+        return -transform.localScale.x > Mathf.Epsilon;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider){
+        //Flip or Turn
+        transform.localScale = new Vector2(Mathf.Sign(enemyRB.velocity.x), transform.localScale.y);
+    }
+
+    public void Die(){
+        Destroy(gameObject);
+    }
+}
